68e2bc44702972a45ee298dd1debea8e2ab092c0
latief13mm <abdullatief.muktimufida@gmail.com>
2021-10-10 13:28:44 +0700

    ini adalah final exam drupal

---
+++ sites/default/files/php/twig/615cfd471bc76_datetime-wrapper.html.twi_LU_2hS4WAq13yj_QBKXdDF7T2/-DBn29-7YA2gSISmLrQft9VsyrMWqGyuaLWqy2Puv1Y.php
@@ -0,0 +1,120 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* themes/restaurant_zymphonies_theme/templates/form/datetime-wrapper.html.twig */
+class __TwigTemplate_d455827a02b5082b922f0510e8273935e80d6f9312cb591287c86b337a97ec7a extends \Twig\Template
+{
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $tags = ["set" => 17, "if" => 23];
+        $filters = ["escape" => 24];
+        $functions = [];
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['set', 'if'],
+                ['escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        // line 17
+        $context["title_classes"] = [0 => "label", 1 => ((        // line 19
+($context["required"] ?? null)) ? ("js-form-required") : ("")), 2 => ((        // line 20
+($context["required"] ?? null)) ? ("form-required") : (""))];
+        // line 23
+        if (($context["title"] ?? null)) {
+            // line 24
+            echo "  <h4";
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute(($context["title_attributes"] ?? null), "addClass", [0 => ($context["title_classes"] ?? null)], "method")), "html", null, true);
+            echo ">";
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["title"] ?? null)), "html", null, true);
+            echo "</h4>
+";
+        }
+        // line 26
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["content"] ?? null)), "html", null, true);
+        echo "
+";
+        // line 27
+        if (($context["errors"] ?? null)) {
+            // line 28
+            echo "  <div class=\"form-item--error-message\">
+    <strong>";
+            // line 29
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["errors"] ?? null)), "html", null, true);
+            echo "</strong>
+  </div>
+";
+        }
+        // line 32
+        if (($context["description"] ?? null)) {
+            // line 33
+            echo "  <div class=\"description\">";
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["description"] ?? null)), "html", null, true);
+            echo "</div>
+";
+        }
+    }
+
+    public function getTemplateName()
+    {
+        return "themes/restaurant_zymphonies_theme/templates/form/datetime-wrapper.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  86 => 33,  84 => 32,  78 => 29,  75 => 28,  73 => 27,  69 => 26,  61 => 24,  59 => 23,  57 => 20,  56 => 19,  55 => 17,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "themes/restaurant_zymphonies_theme/templates/form/datetime-wrapper.html.twig", "C:\\xampp\\htdocs\\profile\\themes\\restaurant_zymphonies_theme\\templates\\form\\datetime-wrapper.html.twig");
+    }
+}
